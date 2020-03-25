6e0e63d6f20a8cce9a23ca4bf122a89c9e5f7154
Thomas Bruederli <thomas@roundcube.net>
2020-03-25 17:36:02 +0100

    Improve Swagger JSON output with additional data from config files
    Instread of parsing the example request/response description blocks, a module can provide
    field definitions with the key `entity` which are considered model data for REST services.
    
    Requires changes in api-tools-documentation:
    thomascube/api-tools-documentation@66228da46
    
    Merges additional specification from each module's `documentation.config.php` file
    into the Swagger JSON output.

--- src/Api.php
+++ src/Api.php
@@ -9,13 +9,14 @@
 namespace Laminas\ApiTools\Documentation\Swagger;
 
 use Laminas\ApiTools\Documentation\Api as BaseApi;
+use Laminas\Stdlib\ArrayUtils;
 
 class Api extends BaseApi
 {
     /**
      * @var BaseApi
      */
-    private $api;
+    protected $api;
 
     /**
      * @param BaseApi $api
@@ -30,17 +31,23 @@ class Api extends BaseApi
      */
     public function toArray()
     {
+        $docsArray = $this->api->getDocs();
+
         $output = [
             'swagger' => '2.0',
             'info' => [
                 'title' => $this->api->getName(),
-                'version' => (string) $this->api->getVersion()
-            ]
+                'version' => (string) $this->api->getVersion(),
+            ],
         ];
 
         foreach ($this->api->services as $service) {
             $outputService = new Service($service);
-            $output = array_merge_recursive($output, $outputService->toArray());
+            $output = ArrayUtils::merge($output, $outputService->toArray());
+        }
+
+        if (isset($docsArray[Api::class])) {
+            $output = ArrayUtils::merge($output, $docsArray[Api::class]);
         }
 
         return $output;
